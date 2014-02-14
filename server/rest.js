89176d763ebe04e2e9d7f3d215ccf1180f1ef2a6
dimapod <Sonrisa50>
2014-02-14 18:13:09 +0100

    Add Mongoose

---
+++ server/rest.js
@@ -0,0 +1,62 @@
+
+module.exports = function (app, mongoose) {
+
+    var Cat = mongoose.model('Cat', { name: String });
+
+    var URL_PREFIX = "/rest";
+
+    app.get(URL_PREFIX + '/systems', function (req, res) {
+        var rez = [
+            {name: "OpenStack", img: "openstack", url: "http://openstack.com", description: "OpenStack is a global collaboration of developers and cloud computing technologists producing the ubiquitous open source cloud computing platform for public and private clouds. The project aims to deliver solutions for all types of clouds by being simple to implement, massively scalable, and feature rich. The technology consists of a series of interrelated projects delivering various components for a cloud infrastructure solution."},
+            {name: "Windows Azure", img: "azure", url: "http://microsoft.com/asure", description: "Windows Azure is an open and flexible cloud platform that enables you to quickly build, deploy and manage applications across a global network of Microsoft-managed datacenters. You can build applications using any language, tool or framework. And you can integrate your public cloud applications with your existing IT environment."},
+            {name: "Microsoft System Center", img: "system_center", url: "http://microsoft.com/systemcenter", description: "System Center 2012 R2 delivers unified management across on-premises, service provider, and Windows Azure environments, thereby enabling the Microsoft Cloud OS. System Center 2012 R2 offers exciting new features and enhancements across infrastructure provisioning, infrastructure monitoring, application performance monitoring, automation and self-service, and IT service management."}
+        ];
+        console.log('/rest/v1.0/compute/system', rez);
+        res.send(rez, 200);
+    });
+
+    app.get(URL_PREFIX + '/cats', function (req, res) {
+        console.log("Get cats");
+        Cat.find({}).exec(function(err, data) {
+            if (err) {
+                console.log("Error: ", err);
+                res.send(500);
+            } else {
+                res.send(data, 200);
+            }
+        });
+    });
+
+    app.post(URL_PREFIX + '/cats', function (req, res) {
+        console.log("Add cat: ", req.body);
+
+        if (req.body.name) {
+            var kitty = new Cat({ name: req.body.name });
+            kitty.save(function (err, doc, id) {
+                if (err) {
+                    console.log("Error: ", err);
+                    res.send(500);
+                } else {
+                    console.log("OK", doc, id);
+                    res.send(201);
+                }
+            });
+        } else {
+            res.send(409);
+        }
+    });
+
+    app.post(URL_PREFIX + '/reinit', function (req, res) {
+        console.log("Reinit base");
+
+        Cat.remove().exec(function(err) {
+            if (err) {
+                console.log("Error: ", err);
+                res.send(500);
+            } else {
+                console.log("OK");
+                res.send(200);
+            }
+        });
+    });
+};
--- server/server.js
+++ server/server.js
@@ -1,18 +1,31 @@
 var express = require('express')
-  , path = require('path')
-  , http = require('http');
+    , path = require('path')
+    , http = require('http')
+    , mongoose = require('mongoose');
+
+//mongoose.connect('mongodb://localhost/ciIntegration');
+mongoose.connect('mongodb://ds033559.mongolab.com:33559/ciintegration', {user: 'test', pass: 'supertest'});
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function callback () {
+    console.log("Connected to mongodb !")
+});
 
 var app = express();
 
-app.configure(function(){
-  app.set('port', process.env.PORT || 8000);
-  app.use(express.static(path.join(__dirname, '../')));
-  app.use(express.bodyParser());
+app.configure(function () {
+    app.set('port', process.env.PORT || 8000);
+    app.use(express.static(path.join(__dirname, '../')));
+    app.use(express.bodyParser());
 });
+
+// REST API
+require('./rest')(app, mongoose);
 
 var server = http.createServer(app);
-server.listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+server.listen(app.get('port'), function () {
+    console.log("Express server listening on port " + app.get('port'));
 });
 
-module.exports=server;
+module.exports = server;
